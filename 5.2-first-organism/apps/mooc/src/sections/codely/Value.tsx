689f2e9d61fb245ecbecda7c11932b261bae1020
Nuria <soriano.nuria@gmail.com>
2022-05-23 12:04:18 +0200

    5.2: first organism end

---
+++ 5.2-first-organism/apps/mooc/src/sections/codely/Value.tsx
@@ -0,0 +1,18 @@
+import { ChatCard } from "@codely/design-system/src/molecules/ChatCard";
+
+import styles from "./Value.module.scss";
+
+export function Value({ cards }: { cards: { title: string; description: string }[] }) {
+  return (
+    <section className={styles.value}>
+      <div className={styles.value__cards}>
+        {cards.map((card) => (
+          <ChatCard key={card.title} title={card.title} description={card.description} />
+        ))}
+      </div>
+      <div className={styles.value__story}>
+        <img src="/story.png" alt="" width={363} height={642} />
+      </div>
+    </section>
+  );
+}
