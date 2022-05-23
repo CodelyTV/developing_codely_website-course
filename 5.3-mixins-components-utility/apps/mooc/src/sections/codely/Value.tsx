99fb8dd1b164f24a5c272bc59a43d7dd662d37b2
Nuria <soriano.nuria@gmail.com>
2022-05-23 12:35:05 +0200

    5.3: mixins vs components vs utility step 1

--- 5.3-mixins-components-utility/apps/mooc/src/sections/codely/Value.tsx
+++ 5.3-mixins-components-utility/apps/mooc/src/sections/codely/Value.tsx
@@ -1,10 +1,13 @@
 import { ChatCard } from "@codely/design-system/src/molecules/ChatCard";
+import layoutStyles from "@codely/design-system/src/objects/Container.module.scss";
+
+import { classNames } from "../shared/classNames";
 
 import styles from "./Value.module.scss";
 
 export function Value({ cards }: { cards: { title: string; description: string }[] }) {
   return (
-    <section className={styles.value}>
+    <section className={classNames(styles.value, layoutStyles.container)}>
       <div className={styles.value__cards}>
         {cards.map((card) => (
           <ChatCard key={card.title} title={card.title} description={card.description} />
