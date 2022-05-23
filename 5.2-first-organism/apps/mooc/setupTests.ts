21c1b67319fe7ac16af7128dee6841270eb3bf7a
Nuria <soriano.nuria@gmail.com>
2022-05-23 11:01:21 +0200

    5.2: first organism start

---
+++ 5.2-first-organism/apps/mooc/setupTests.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata";
+import "@testing-library/jest-dom";
+
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: () => {
+    return "Next image stub";
+  },
+}));
+
+Element.prototype.scrollTo = jest.fn();
+
+class IntersectionObserver {
+  observe = jest.fn();
+  disconnect = jest.fn();
+  unobserve = jest.fn();
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+Object.defineProperty(global, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
