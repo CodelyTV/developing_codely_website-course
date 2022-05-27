73b4054b0185d510f738c76dcf66718f89615559
Nuria <soriano.nuria@gmail.com>
2022-05-27 14:33:14 +0200

    7.2: flex vs grid start

---
+++ 7.2-flex-grid/apps/mooc/src/pages/index.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+
+import { KeyPoints } from "../sections/codely/KeyPoints";
+import { Value } from "../sections/codely/Value";
+import { Changelog } from "../sections/contents/Changelog";
+
+const Home: NextPage = () => {
+  const valueCards = [
+    {
+      title: "🌈 Mucho más que cursos",
+      description: "Aprende de casos de El Mundo Real™️ con las auditorías, lightning talks, entrevistas…",
+    },
+    {
+      title: "✌️ No venimos a sentar cátedra",
+      description: "Hacemos el contenido y formato que nos hubiera gustado tener.",
+    },
+    {
+      title: "⚡ Acelera tu aprendizaje",
+      description: "Aprovecha el conocimiento de profesionales con una gran experiencia.",
+    },
+    {
+      title: "🚀 Crece en equipo con tu empresa",
+      description: "Sube de nivel para ponerte a punto y encarar los nuevos retos conjuntamente.",
+    },
+  ];
+
+  return (
+    <>
+      <KeyPoints />
+      <Changelog />
+      <Value cards={valueCards} />
+    </>
+  );
+};
+
+export default Home;
