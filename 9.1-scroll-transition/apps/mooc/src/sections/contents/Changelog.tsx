31e34c83db302b92d6d91842cd7e89cff619f78f
Nuria <soriano.nuria@gmail.com>
2022-05-30 14:52:33 +0200

    9.1: scroll transition end

--- 9.1-scroll-transition/apps/mooc/src/sections/contents/Changelog.tsx
+++ 9.1-scroll-transition/apps/mooc/src/sections/contents/Changelog.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react";
+
 import { Heading } from "@codely/design-system/src/atoms/Heading";
 import { Link } from "@codely/design-system/src/atoms/Link";
 import { ChangelogCard } from "@codely/design-system/src/molecules/ChangelogCard";
@@ -9,10 +11,38 @@ import styles from "./Changelog.module.scss";
 
 export function Changelog() {
   const changelog = useChangelog();
+  const container = useRef<HTMLDivElement>(null);
+
+  const [parallaxVariables, setParallaxVariables] = useState({
+    top: 0,
+    height: 0,
+  });
+
+  function parallax() {
+    if (!container.current) {
+      return;
+    }
+
+    const { top, height } = container.current?.getBoundingClientRect();
+
+    setParallaxVariables({ top, height });
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", parallax);
+
+    return () => {
+      document.removeEventListener("scroll", parallax);
+    };
+  }, []);
 
   return (
     <Container tag="section" ariaLabelledby="changelog-title">
-      <div className={styles.changelog}>
+      <div
+        ref={container}
+        className={styles.changelog}
+        style={{ "--top": parallaxVariables.top, "--height": parallaxVariables.height } as React.CSSProperties}
+      >
         <div className={styles.changelog__content}>
           <div className={styles.changelog__title}>
             <Heading id="changelog-title" size="medium" level="h2">
