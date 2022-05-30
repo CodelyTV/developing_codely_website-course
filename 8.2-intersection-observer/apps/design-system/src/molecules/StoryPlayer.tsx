29ac817fa3a75032d685e712ce331c7e30b56ce7
Nuria <soriano.nuria@gmail.com>
2022-05-30 12:31:40 +0200

    8.2: intersection observer end

--- 8.2-intersection-observer/apps/design-system/src/molecules/StoryPlayer.tsx
+++ 8.2-intersection-observer/apps/design-system/src/molecules/StoryPlayer.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import styles from "./StoryPlayer.module.scss";
 
 export interface Story {
@@ -11,20 +13,67 @@ export interface StoryPlayerProps {
 
 export function StoryPlayer({ stories }: StoryPlayerProps) {
   const story = stories[0];
+  const video = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (!video.current) {
+      return;
+    }
+
+    const playObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const video = entry.target as HTMLVideoElement;
+          if (entry.isIntersecting) {
+            video.play().catch(() => {
+              console.error("can't play video");
+            });
+
+            return;
+          }
+
+          video.pause();
+        });
+      },
+      { threshold: 0.75 }
+    );
+
+    const loadObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const video = entry.target as HTMLVideoElement;
+
+            for (const source in video.children) {
+              const videoSource = video.children[source] as HTMLSourceElement;
+              if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                videoSource.src = videoSource.dataset.src ?? "";
+              }
+            }
+
+            video.load();
+            observer.unobserve(video);
+            playObserver.observe(video);
+
+            return;
+          }
+        });
+      },
+      { threshold: 0 }
+    );
+
+    loadObserver.observe(video.current);
+
+    return () => {
+      loadObserver.disconnect();
+      playObserver.disconnect();
+    };
+  });
 
   return (
     <section className={styles.player}>
-      <video
-        src={story.videoSrc}
-        width="363"
-        height="678"
-        muted
-        loop
-        poster={story.posterSrc}
-        playsInline
-        tabIndex={-1}
-        autoPlay
-      >
+      <video ref={video} width="363" height="678" muted loop poster={story.posterSrc} playsInline tabIndex={-1}>
+        <source data-src={story.videoSrc} type="video/mp4" />
         Sorry, your browser does not support embedded videos.
       </video>
     </section>
